76dc2faa0ba53d94c379d4df4e3311e4e4d26f40
SHARON GONZALEZ <fionna@MacBook-Air-89.local>
2025-10-19 23:12:52 -0700

    added txt file and input answers in js too

---
+++ app.js
@@ -0,0 +1,13 @@
+let partyLocation = "Garden" //string; declared 'let' since the location can change
+
+let shiftValue = 3 //number; 'const' since the number is constant throughout
+
+let encodedMessage = ""; //string; 'let' because message will change when when solved
+
+let decodedMessage = ""; //string; 'let' because message will change to show secret location
+
+let isPartySecure = false; //boolean; 'let'because it may or may not change 
+
+
+const shiftValue = 3;
+Number.isInteger(3);
